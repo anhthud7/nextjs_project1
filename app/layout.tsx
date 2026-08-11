e6372fc43d99f0f130fb521cd578e75d48924700
anhthud7 <ngathud7@gmail.com>
2026-08-11 17:27:16 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+        <h1> this is from layout.tsx </h1>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <div>
+            <h1> this is my first page </h1>
+        </div>
+    );
+}
